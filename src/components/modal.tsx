7c9cc2131dd409dc62ccedc16ed6bc4f38a2ebd7
JHSeo <qnfqnfqnf@gmail.com>
2023-05-07 20:31:07 +0900

    feat: add Portal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Portal } from '@radix-ui/react-portal';
 import { useRouter } from 'next/navigation';
 
 interface ModalProps {
@@ -9,14 +10,16 @@ interface ModalProps {
 function Modal({ children }: ModalProps) {
   const router = useRouter();
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
-      {children}
-      <button
-        type="button"
-        className="absolute inset-0 z-[-1] bg-gray-900/80 backdrop-blur-sm"
-        onClick={() => router.back()}
-      />
-    </div>
+    <Portal asChild>
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        {children}
+        <button
+          type="button"
+          className="absolute inset-0 z-[-1] bg-gray-900/80 backdrop-blur-sm"
+          onClick={() => router.back()}
+        />
+      </div>
+    </Portal>
   );
 }
 
